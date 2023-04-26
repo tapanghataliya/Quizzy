b0fefccc30a25afeb17db467da4c42701544cd2f
tapang <tapang@mindfiresolutions.com>
2023-04-26 10:30:52 +0530

    Remove extra files and code.

--- app/src/main/java/com/example/quizzy/features/settings/data/repository/SettingsRepositoryImpl.kt
+++ app/src/main/java/com/example/quizzy/features/settings/data/repository/SettingsRepositoryImpl.kt
@@ -1,20 +1,14 @@
 package com.example.quizzy.features.settings.data.repository
 
-import android.content.Context
 import android.os.Build
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.example.quizzy.core.remote.ApiService
-import com.example.quizzy.core.utils.Constant
-import com.example.quizzy.core.utils.NetworkUtils
 import com.example.quizzy.core.utils.Resource
 import com.example.quizzy.features.settings.data.model.TriviaCategory
 import com.example.quizzy.features.settings.domain.repository.SettingsRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 
 class SettingsRepositoryImpl(
-    private val apiService: ApiService,
-    @ApplicationContext private val context: Context
+    private val apiService: ApiService
 ) : SettingsRepository {
     @RequiresApi(Build.VERSION_CODES.M)
     override suspend fun getCategory(): Resource<List<TriviaCategory>> {
